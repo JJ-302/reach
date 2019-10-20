6648b4c513bbd5cdf36c25c1592a387bd27f5d12
Jun <>
2019-10-20 12:53:06 +0900

    remove semi

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from '../logo.svg';
-import '../css/App.scss';
+import React from 'react'
+import logo from '../logo.svg'
+import '../css/App.scss'
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
