61588e550d7584fda85c67b944e2150c99aeded1
Jun <>
2019-10-20 13:39:47 +0900

    preparing for use to fontawesome

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
+
+library.add(fab, fas, far)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
