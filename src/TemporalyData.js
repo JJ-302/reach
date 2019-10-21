e476942496afb133c68e572bab6b3dd7847420a3
Jun <>
2019-10-21 20:15:59 +0900

    add data to TemporalyData.js

--- src/TemporalyData.js
+++ src/TemporalyData.js
@@ -82,5 +82,171 @@ export default [
         ],
       },
     ],
+  }, {
+    name: 'project_3',
+    members: [
+      { name: 'member_1' },
+      { name: 'member_2' },
+    ],
+    tasks: [
+      {
+        id: '7',
+        title: 'task_7',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_1' },
+        ],
+      }, {
+        id: '8',
+        title: 'task_9',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_2' },
+        ],
+      }, {
+        id: '10',
+        title: 'task_10',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_1' },
+          { name: 'member_2' },
+        ],
+      },
+    ],
+  }, {
+    name: 'project_4',
+    members: [
+      { name: 'member_2' },
+      { name: 'member_3' },
+      { name: 'member_4' },
+    ],
+    tasks: [
+      {
+        id: '11',
+        title: 'task_11',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_2' },
+        ],
+      }, {
+        id: '12',
+        title: 'task_13',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_2' },
+          { name: 'member_3' },
+        ],
+      }, {
+        id: '13',
+        title: 'task_13',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_2' },
+          { name: 'member_3' },
+          { name: 'member_4' },
+        ],
+      },
+    ],
+  }, {
+    name: 'project_5',
+    members: [
+      { name: 'member_1' },
+      { name: 'member_2' },
+    ],
+    tasks: [
+      {
+        id: '14',
+        title: 'task_14',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_1' },
+        ],
+      }, {
+        id: '15',
+        title: 'task_15',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_2' },
+        ],
+      }, {
+        id: '16',
+        title: 'task_16',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_1' },
+          { name: 'member_2' },
+        ],
+      },
+    ],
+  }, {
+    name: 'project_6',
+    members: [
+      { name: 'member_2' },
+      { name: 'member_3' },
+      { name: 'member_4' },
+    ],
+    tasks: [
+      {
+        id: '17',
+        title: 'task_17',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_2' },
+        ],
+      }, {
+        id: '18',
+        title: 'task_18',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_2' },
+          { name: 'member_3' },
+        ],
+      }, {
+        id: '19',
+        title: 'task_19',
+        startDate: '2019/11/01',
+        endDate: '2019/11/30',
+        duration: 30,
+        percentComplete: 50,
+        member_tasks: [
+          { name: 'member_2' },
+          { name: 'member_3' },
+          { name: 'member_4' },
+        ],
+      },
+    ],
   },
 ]
