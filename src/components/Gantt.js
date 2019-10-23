5f775622138f5e7d7fb03937efa62639a2d0c42f
Jun <>
2019-10-23 12:11:10 +0900

    create gantt view

--- src/components/Gantt.js
+++ src/components/Gantt.js
@@ -1,24 +1,41 @@
 import React from 'react'
+import Moment from 'moment'
 import '../css/Project.scss'
 
+const baseChartWidth = 35
+
+const calcOffset = (content) => {
+  const start = Moment('2019-01-01')
+  const offset = Moment(content.startDate, 'YYYY/MM/DD').diff(start, 'days') * baseChartWidth
+  return offset
+}
+
 const Gantt = (props) => {
-  const projects = props.projects.map((project) => (
-    <div key={project.name} className="project--gantt">
-      <div className="project__chart">
-        {project.name}
+  const projects = props.projects.map((project) => {
+    const chartWidth = baseChartWidth * project.duration
+    const offset = calcOffset(project)
+    return (
+      <div key={project.name} className="project--gantt">
+        <div style={{ paddingLeft: offset }} className="project__name">
+          <div style={{ width: chartWidth }} className="project__chart" />
+        </div>
+        <Task tasks={project.tasks} />
       </div>
-      <Task tasks={project.tasks} />
-    </div>
-  ))
+    )
+  })
   return projects
 }
 
 const Task = (props) => {
-  const tasks = props.tasks.map((task) => (
-    <div key={task.id} className="task">
-      <div className="task__name">{task.title}</div>
-    </div>
-  ))
+  const tasks = props.tasks.map((task) => {
+    const chartWidth = baseChartWidth * task.duration
+    const offset = calcOffset(task)
+    return (
+      <div key={task.id} style={{ paddingLeft: offset }} className="task">
+        <div style={{ width: chartWidth }} className="task__chart" />
+      </div>
+    )
+  })
   return tasks
 }
 
