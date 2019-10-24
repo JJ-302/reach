27d6fca6d38d1a9eb3f852dc360dcfcbf7c703e2
Jun <>
2019-10-24 09:39:37 +0900

    add config of routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import SideBar from './SideBar'
 import Main from './Main'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
 import '../css/App.scss'
 
-const App = () => (
+const Home = () => (
   <div className="App">
     <SideBar />
     <Main />
   </div>
+)
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/"><SignIn /></Route>
+      <Route path="/signup"><SignUp /></Route>
+      <Route path="/home"><Home /></Route>
+      <Route exact><SignIn /></Route>
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
