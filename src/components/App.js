47f94c8b1ee3b855f9aeada02a0a521de663e567
Jun <>
2019-10-27 22:08:59 +0900

    if authentication failed then redirect to sign in

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React, { Component } from 'react'
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
 
 import SideBar from './SideBar'
 import Main from './Main'
@@ -7,19 +12,35 @@ import SignIn from './SignIn'
 import SignUp from './SignUp'
 import '../css/App.scss'
 
-const Home = () => (
-  <div className="App">
-    <SideBar />
-    <Main />
-  </div>
-)
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    const token = localStorage.getItem('token')
+    const isAuthenticated = token !== null
+    this.state = {
+      isAuthenticated,
+    }
+  }
+
+  render() {
+    const { isAuthenticated } = this.state
+    return (
+      isAuthenticated ? (
+        <div className="App">
+          <SideBar />
+          <Main />
+        </div>
+      ) : <Redirect to="/signin" />
+    )
+  }
+}
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/"><SignIn /></Route>
+      <Route path="/signin"><SignIn /></Route>
       <Route path="/signup"><SignUp /></Route>
-      <Route path="/home"><Home /></Route>
+      <Route exact path="/"><Home /></Route>
       <Route exact><SignIn /></Route>
     </Switch>
   </BrowserRouter>
