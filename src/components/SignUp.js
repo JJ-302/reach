64d0784bbbefc1114e27e5c1a422c6e643c37c94
Jun <>
2019-10-27 22:00:36 +0900

    implement sign up

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,64 +1,153 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import ErrorMessage from './Error'
+import Utils from '../Utils'
 import '../css/Session.scss'
 
 export default class SignUp extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      uri: '',
+      avatar: null,
+      name: '',
       email: '',
       password: '',
       passwordConfirmation: '',
+      errors: [],
+      isSignIn: false,
     }
   }
 
-  emailOnChange = (event) => {
+  handleSignUp = () => {
+    const {
+      avatar,
+      name,
+      email,
+      password,
+      passwordConfirmation,
+    } = this.state
+
+    const params = new FormData()
+    params.append('user[name]', name)
+    params.append('user[email]', email)
+    params.append('user[password]', password)
+    params.append('user[password_confirmation]', passwordConfirmation)
+    if (avatar) {
+      params.append('user[avatar]', avatar)
+    }
+
+    const url = Utils.buildRequestUrl('/users')
+    fetch(url, {
+      method: 'POST',
+      body: params,
+    })
+      .then((_res) => _res.json())
+      .then((res) => {
+        if (res.is_created) {
+          localStorage.setItem('token', res.token)
+          this.setState({ isSignIn: true })
+        } else {
+          this.setState({ errors: res.errors })
+        }
+      })
+      .catch(() => {
+        // TODO
+      })
+  }
+
+  onChangeFile = (event) => {
+    const avatar = event.target.files[0]
+    const uri = URL.createObjectURL(avatar)
+    this.setState({ uri, avatar })
+  }
+
+  onChangeName = (event) => {
+    const name = event.target.value
+    this.setState({ name })
+  }
+
+  onChangeEmail = (event) => {
     const email = event.target.value
     this.setState({ email })
   }
 
-  passwordOnChange = (event) => {
+  onChangePassword = (event) => {
     const password = event.target.value
     this.setState({ password })
   }
 
-  passwordConfirmationOnChange = (event) => {
+  onChangePasswordConfirmation = (event) => {
     const passwordConfirmation = event.target.value
     this.setState({ passwordConfirmation })
   }
 
   render() {
-    const { email, password, passwordConfirmation } = this.state
+    const {
+      uri,
+      name,
+      email,
+      password,
+      passwordConfirmation,
+      errors,
+      isSignIn,
+    } = this.state
+
     return (
-      <div className="background">
-        <div className="session">
-          <div className="session__title">Create account</div>
-          <input
-            type="text"
-            className="session__email"
-            placeholder="Email"
-            value={email}
-            onChange={this.emailOnChange}
-          />
-          <input
-            type="password"
-            className="session__password"
-            placeholder="Password"
-            value={password}
-            onChange={this.passwordOnChange}
-          />
-          <input
-            type="password"
-            className="session__password"
-            placeholder="Password confirmation"
-            value={passwordConfirmation}
-            onChange={this.passwordConfirmationOnChange}
-          />
-          <Link to="/home" className="session__submit">Sign Up</Link>
-          <Link to="/" className="session__switch">Move to Sign In</Link>
+      isSignIn ? <Redirect to="/" /> : (
+        <div className="background">
+          <div className="session">
+            <div className="session__title">Create account</div>
+            {errors.length !== 0 && <ErrorMessage action="Registration" errors={errors} />}
+            <label htmlFor="avatarForm" className="session__avatar">
+              <input
+                id="avatarForm"
+                type="file"
+                className="session__fileField"
+                onChange={this.onChangeFile}
+              />
+              {uri === ''
+                ? <FontAwesomeIcon icon={['fas', 'user']} className="session__fileIcon" />
+                : <img src={uri} alt="avatar" className="session__preview" />}
+            </label>
+            <div className="session__fileLabel">Upload your avatar</div>
+            <input
+              type="text"
+              className="session__name"
+              placeholder="Name"
+              value={name}
+              onChange={this.onChangeName}
+            />
+            <input
+              type="text"
+              className="session__email"
+              placeholder="Email"
+              value={email}
+              onChange={this.onChangeEmail}
+            />
+            <input
+              type="password"
+              className="session__password"
+              placeholder="Password"
+              value={password}
+              onChange={this.onChangePassword}
+            />
+            <input
+              type="password"
+              className="session__password"
+              placeholder="Password confirmation"
+              value={passwordConfirmation}
+              onChange={this.onChangePasswordConfirmation}
+            />
+            <button type="button" onClick={this.handleSignUp} className="session__submit">
+              Sign Up
+            </button>
+            <Link to="/signin" className="session__switch">Move to Sign In</Link>
+          </div>
         </div>
-      </div>
+      )
     )
   }
 }
