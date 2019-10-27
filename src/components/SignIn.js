e47a2615daade382b95a63801d7362a69d8dbdca
Jun <>
2019-10-27 23:47:26 +0900

    implement sign in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 
+import ErrorMessage from './Error'
+import Utils from '../Utils'
 import '../css/Session.scss'
 
 export default class SignIn extends Component {
@@ -9,7 +11,36 @@ export default class SignIn extends Component {
     this.state = {
       email: '',
       password: '',
+      isSignIn: false,
+      errors: [],
     }
+  }
+
+  handleSignIn = () => {
+    const { email, password } = this.state
+    const url = Utils.buildRequestUrl('/sessions')
+    const params = {
+      email,
+      password,
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(params),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((_res) => _res.json())
+      .then((res) => {
+        const { result, token, errors } = res
+        if (result) {
+          localStorage.setItem('token', token)
+          this.setState({ isSignIn: true })
+        } else {
+          this.setState({ errors })
+        }
+      })
+      .catch(() => {
+        // TODO
+      })
   }
 
   emailOnChange = (event) => {
@@ -23,29 +54,40 @@ export default class SignIn extends Component {
   }
 
   render() {
-    const { email, password } = this.state
+    const {
+      email,
+      password,
+      isSignIn,
+      errors,
+    } = this.state
+
     return (
-      <div className="background">
-        <div className="session">
-          <div className="session__title">Sign in to Reach</div>
-          <input
-            type="text"
-            className="session__email"
-            placeholder="Email"
-            value={email}
-            onChange={this.emailOnChange}
-          />
-          <input
-            type="password"
-            className="session__password"
-            placeholder="Password"
-            value={password}
-            onChange={this.passwordOnChange}
-          />
-          <Link to="/home" className="session__submit">Sign In</Link>
-          <Link to="/signup" className="session__switch">Create account</Link>
+      isSignIn ? <Redirect to="/" /> : (
+        <div className="background">
+          <div className="session">
+            <div className="session__title">Sign in to Reach</div>
+            {errors.length !== 0 && <ErrorMessage action="Authentication" errors={errors} />}
+            <input
+              type="text"
+              className="session__email"
+              placeholder="Email"
+              value={email}
+              onChange={this.emailOnChange}
+            />
+            <input
+              type="password"
+              className="session__password"
+              placeholder="Password"
+              value={password}
+              onChange={this.passwordOnChange}
+            />
+            <button type="button" onClick={this.handleSignIn} className="session__submit">
+              Sign In
+            </button>
+            <Link to="/signup" className="session__switch">Create account</Link>
+          </div>
         </div>
-      </div>
+      )
     )
   }
 }
