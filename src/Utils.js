7134f9602197caa6e68e35e194c65f713bcbc4bf
Jun <>
2019-11-03 12:50:24 +0900

    add function in order to return dateRange

--- src/Utils.js
+++ src/Utils.js
@@ -1,6 +1,16 @@
+import Moment from 'moment'
+
 export default class Utils {
   static buildRequestUrl(apiEndpoint) {
     const baseUrl = 'http://localhost:3001'
     return baseUrl + apiEndpoint
+  }
+
+  static dateRangeStart() {
+    return Moment(new Date()).subtract(2, 'weeks')
+  }
+
+  static dateRangeEnd() {
+    return Moment('2020/4/1', 'YYYY/MM/DD')
   }
 }
