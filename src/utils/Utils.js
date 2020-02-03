4b07d2c5d96ef2294d34b21e8cec408fbd111830
Jun <>
2020-02-03 10:20:42 +0900

    edit request endpoint for local development

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -2,7 +2,8 @@ import Moment from 'moment'
 
 export default class Utils {
   static buildRequestUrl(apiEndpoint) {
-    const baseUrl = 'https://reach-260801.appspot.com'
+    // const baseUrl = 'https://reach-260801.appspot.com'
+    const baseUrl = 'http://localhost:3001'
     return baseUrl + apiEndpoint
   }
 
