eb30a67fa6f957a4d8026dc04d06fbd50bbbfa08
Jun <>
2020-02-04 12:34:53 +0900

    fix code style for index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.scss'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
-import App from './components/App'
-import * as serviceWorker from './serviceWorker'
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
 
-library.add(fab, fas, far)
+library.add(fab, fas, far);
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
