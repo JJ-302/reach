a00bf6a2e16d079e2cccd1dbab161f17c7d71a77
Jun <cornerstone.am3@gmail.com>
2020-02-22 12:21:37 +0900

    edit production URI

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,7 +1,7 @@
 export default class Utils {
   static buildRequestUrl(apiEndpoint) {
-    // const baseUrl = 'https://reach-260801.appspot.com'
-    const baseUrl = 'http://localhost:3001';
+    const baseUrl = 'https://api-reach.herokuapp.com';
+    // const baseUrl = 'http://localhost:3001';
     return baseUrl + apiEndpoint;
   }
 }
